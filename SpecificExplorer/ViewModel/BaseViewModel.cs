897e4c71feafaa4ff3fdde8f912f84be639ea403
Tobias Stroedicke <tobis-stroedicke@gmx.de>
2024-04-30 20:30:40 +0200

    Add MainViewModel

--- SpecificExplorer/ViewModel/BaseViewModel.cs
+++ SpecificExplorer/ViewModel/BaseViewModel.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2Duzz.ViewModels
+namespace SpecificExplorer.ViewModel
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
---
+++ SpecificExplorer/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecificExplorer.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+    }
+}
