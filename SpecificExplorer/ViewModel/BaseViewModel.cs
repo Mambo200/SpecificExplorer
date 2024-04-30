553061af4cd037aa1fed805f950a0710b3d62447
Tobias Stroedicke <tobis-stroedicke@gmx.de>
2024-04-30 20:15:17 +0200

    Create Base Viewmodel

---
+++ SpecificExplorer/ViewModel/BaseViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Duzz.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyOfPropertyChange([CallerMemberName] string _propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_propertyName));
+            }
+        }
+
+        protected void SetProperty<T>(ref T _propertyReference, T value, [CallerMemberName] string _propertyName = "")
+        {
+
+
+            if (EqualityComparer<T>.Default.Equals(_propertyReference, value))
+                return;
+
+            //if value changed but didn't validate
+            //if (validateValue != null )
+            //	return;
+
+            _propertyReference = value;
+            NotifyOfPropertyChange(_propertyName);
+        }
+    }
+}
